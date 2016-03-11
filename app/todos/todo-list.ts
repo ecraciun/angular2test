b4587623ab77b7a3437402b8ef873fe8426d9b0c
Emil Craciun <emil.craciun@visma.com>
2016-03-11 16:53:02 +0200

    Created custom pipe (filter)

--- app/todos/todo-list.ts
+++ app/todos/todo-list.ts
@@ -1,14 +1,16 @@
 import {Component, Inject} from 'angular2/core';
 import {TodoService}  from './todo-service'
 import {TodoItemRenderer} from './todo-item-renderer';
+import {SearchPipe} from './search-pipe';
 
 
 @Component({
     selector: 'todo-list',
     directives: [TodoItemRenderer],
+    pipes: [SearchPipe],
     template: `
     <ul>
-        <li *ngFor="#todo of todoService.todos">
+        <li *ngFor="#todo of todoService.todos | search">
             <todo-item-renderer [todo]="todo"></todo-item-renderer>
         </li>
     </ul>
