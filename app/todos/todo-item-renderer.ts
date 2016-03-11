f92efd88189a98f6b4e1b2c79f83314074e86183
Emil Craciun <emil.craciun@visma.com>
2016-03-11 16:49:14 +0200

    view encapsulation

--- app/todos/todo-item-renderer.ts
+++ app/todos/todo-item-renderer.ts
@@ -1,8 +1,12 @@
 import {Component, Input} from 'angular2/core';
 import {TodoService}  from './todo-service'
+import {ViewEncapsulation} from 'angular2/core';
 
 @Component({
     selector: 'todo-item-renderer',
+    //encapsulation: ViewEncapsulation.None, // all in, all out
+    //encapsulation: ViewEncapsulation.Native, // no styles in, no styles out
+    encapsulation: ViewEncapsulation.Emulated, // all in, none out
     template: `
     <style>
         .completed{
