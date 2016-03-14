190dd639bf8f34993c10b12a7ba20102810a1891
Emil Craciun <emil.craciun@visma.com>
2016-03-14 10:28:51 +0200

    added dashboard component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,29 @@ import {StatusSelector}     from './todos/todo/components/status-selector';
 import {SearchBox}          from './todos/search/components/search-box';
 import {HeroesComponent}    from './heroes/components/heroes.component';
 import {HeroService}        from './heroes/services/hero.service';
+import {DashboardComponent} from './dashboard.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS }     from 'angular2/router';
 
 
 @Component({
     selector: 'my-app',
-    directives: [TodoListItem, TodoList, StatusSelector, SearchBox, HeroesComponent],
+    directives: [TodoListItem, TodoList, StatusSelector, SearchBox, ROUTER_DIRECTIVES],
     templateUrl: '/app/app.component.html',
-    providers: [HeroService]
+    providers: [HeroService, ROUTER_PROVIDERS]
 })
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    }
+])
 export class AppComponent { 
     public title = 'Tour of Heroes';
 }
